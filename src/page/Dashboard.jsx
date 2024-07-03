9bc6d96bef3ed97d3ae071f6764baaa4383f7f48
sardor1411 <shahnozka1104@gmail.com>
2024-07-03 16:53:41 +0500

    1

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -311,15 +311,18 @@ function Dashboard() {
           </div>
         )}
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4 mb-[50px]">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {box.map((mall) => (
-          <div className="border border-black w-[70%] max-h-full text-center m-auto h-auto rounded-md mt-6 shadow-lg" key={mall.id}>
+          <div className="border border-black max-w-full max-h-full text-center m-auto rounded-md h-auto mt-6 shadow-lg" key={mall.id}>
             <div className="box p-4">
-              <img className="w-[52%] m-auto h-[250px] rounded-t-md" src={mall.img} alt={mall.title} />
+              <img className="w-full h-64 object-cover rounded-t-md" src={mall.img} alt={mall.title} />
               <p className="mt-6 mb-4 text-[20px] text-blue-500">Zakaz vaqti: {mall.firstData}</p>
               <p className="mt-6 text-[21px] text-green-600 font-semibold">To'yxona haqida: {mall.title}</p>
-              <p className="mt-6 text-[21px] text-green-600 font-semibold">Montajchi: {mall.bekzod}</p>
-              <p className="mt-6 text-[21px] text-green-600 font-semibold">Montajchi: {mall.siroj}</p>
+              <div className="border rounded-[10px] shadow-inner">
+                <h2 className="text-[18px]">Montajchilar ro'yxati:</h2>
+                <p>{mall.bekzod}</p>
+                <p>{mall.siroj}</p>
+              </div>
 
               <p className="mt-6 mb-4 text-[22px] text-red-500">To'y xaqida ma'lumot:</p>
               <textarea
