a42f1c888293fb7af472b09b2d59c4c365f6d87f
sardor1411 <shahnozka1104@gmail.com>
2024-07-09 11:24:13 +0500

    1

--- src/protectedRoutes/ProtectRouteBekzod.jsx
+++ src/protectedRoutes/ProtectRouteBekzod.jsx
@@ -1,13 +1,13 @@
 import { Navigate } from "react-router-dom";
-import { Bekzod } from "../page/Bekzod";
 
 export const ProtectRouteBekzod = ({children}) => {
-    let user = JSON.parse(localStorage.getItem("users"));
-
-    if (user && user.role === "bekzod" || user.role === "admin") {
+    let user = localStorage.getItem("users");
+    if (user) {
+        user = JSON.parse(user);
+    }
+    if (user && (user.role === "bekzod" )) {
         return children;
     } else {
-        console.log('sa');
-        return <Navigate to={'/signin'} />;
+        return <Navigate to='/signin' />;
     }
 };
